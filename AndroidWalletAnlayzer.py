31c9cb184c0e4b95251e9f0e144c3e43484ffad4
ironmask1204 <ironmask1204@gmail.com>
2023-11-03 17:37:33 +0900

    UI improvement with exception handling

--- AndroidWalletAnlayzer.py
+++ AndroidWalletAnlayzer.py
@@ -19,10 +19,11 @@ def openweb():
 
 def main():
     window = Tk()                                               # 창 생성
-    window.geometry("500x820")                                  # 창 크기설정
+    window.geometry("500x900")                                  # 창 크기설정
     window.title("AndroidWalletAnalyzer by ironmask")           # 창 제목
     window.option_add("*Font","맑은고딕 12")                    # 폰트
     window.resizable(False, False)                              # x, y 창 크기 고정
+
 
 #=================================================================================
 
@@ -31,18 +32,33 @@ def main():
         window.exodusFilePath = filedialog.askopenfilename(initialdir= os.path.dirname(__file__))    
         exodusFileLabel.configure(text = window.exodusFilePath)
 
+
     # 전체 레이블
     exodusLabel = Label(window, width=50, height=10, fg="red", relief="solid")
     exodusLabel.pack(pady=5)
 
+    # 부분 레이블
+    exodusLabel2 = Label(exodusLabel, width=50, height=5, fg="red", relief="solid")
+    exodusLabel2.pack(pady=5)
 
     # 파일 열기 버튼
-    button = Button(exodusLabel, text="Exodus db 파일 열기", command=fileOpenDialog_exodus, width=30, bg='yellow')
-    button.pack(pady=5)
+    button1 = Button(exodusLabel2, text="Open Exodus db file", command=fileOpenDialog_exodus, width=30, bg='yellow', state='disabled')
+    button1.pack(pady=5, side=LEFT)
+
+    def chk1proc():
+        if(var1.get()==1):
+            button1.config(state='active')
+        else:
+            button1.config(state='disabled')
+
+    # check box
+    var1 = IntVar()
+    c1 = Checkbutton(exodusLabel2, text='', variable=var1, command=chk1proc)
+    c1.pack(side=RIGHT)
 
 
     # Exodus 파일 경로 표시
-    exodusFileLabel = Label(exodusLabel, text="Exodus db 파일", width=40, height=3, fg="red", relief="solid")
+    exodusFileLabel = Label(exodusLabel, text="Exodus db file", width=40, height=3, fg="red", relief="solid")
     #label.grid(column=0, row=0)
     exodusFileLabel.pack(pady=5)  
 #=================================================================================    
@@ -56,13 +72,30 @@ def main():
     bitgetLabel = Label(window, width=50, height=10, fg="red", relief="solid")
     bitgetLabel.pack(pady=5)
 
+    # 부분 레이블
+    bitgetLabel2 = Label(bitgetLabel, width=50, height=5, fg="red", relief="solid")
+    bitgetLabel2.pack(pady=5)
+
     # 파일 열기 버튼
-    button = Button(bitgetLabel, text="Bitget_Wallet db 파일 열기", command=fileOpenDialog_Bitget_Wallet, width=30, bg='yellow')
-    button.pack(pady=5)
+    button2 = Button(bitgetLabel2, text="Open Bitget_Wallet db file", command=fileOpenDialog_Bitget_Wallet, width=30, bg='yellow', state='disabled')
+    button2.pack(pady=5, side=LEFT)
+
+    def chk2proc():
+        if(var2.get()==1):
+            button2.config(state='active')
+        else:
+            button2.config(state='disabled')    
+
+    # check box
+    var2 = IntVar()
+    c2 = Checkbutton(bitgetLabel2, text='', variable=var2, command=chk2proc)
+    c2.pack(side=RIGHT)
+
 
     # Bitget_Wallet 파일 경로 표시
-    bitgetFileLabel = Label(bitgetLabel, text="Bitget_Wallet db 파일", width=40, height=3, fg="red", relief="solid")
+    bitgetFileLabel = Label(bitgetLabel, text="Bitget_Wallet db file", width=40, height=3, fg="red", relief="solid")
     bitgetFileLabel.pack(pady=5)
+
 #=================================================================================
     
     # Bitpay 파일 열기 버튼 클릭시 호출
@@ -74,12 +107,27 @@ def main():
     bitpayLabel = Label(window, width=50, height=10, fg="red", relief="solid")
     bitpayLabel.pack(pady=5)
 
+    # 부분 레이블
+    bitpayLabel2 = Label(bitpayLabel, width=50, height=5, fg="red", relief="solid")
+    bitpayLabel2.pack(pady=5)
+
     # 파일 열기 버튼
-    button = Button(bitpayLabel, text="Bitpay db 파일 열기", command=fileOpenDialog_Bitpay, width=30, bg='yellow')
-    button.pack(pady=5)
+    button3 = Button(bitpayLabel2, text="Open Bitpay db file", command=fileOpenDialog_Bitpay, width=30, bg='yellow', state='disabled')
+    button3.pack(pady=5, side=LEFT)
+
+    def chk3proc():
+        if(var3.get()==1):
+            button3.config(state='active')
+        else:
+            button3.config(state='disabled')   
+    
+    # check box
+    var3 = IntVar()
+    c3 = Checkbutton(bitpayLabel2, text='', variable=var3, command=chk3proc)
+    c3.pack(side=RIGHT)    
 
     # Bitpay 파일 경로 표시
-    bitpayFileLabel = Label(bitpayLabel, text="Bitpay db 파일", width=40, height=3, fg="red", relief="solid")
+    bitpayFileLabel = Label(bitpayLabel, text="Bitpay db file", width=40, height=3, fg="red", relief="solid")
     bitpayFileLabel.pack(pady=5)
 #=================================================================================
     
@@ -92,12 +140,27 @@ def main():
     tokenpocketLabel = Label(window, width=50, height=10, fg="red", relief="solid")
     tokenpocketLabel.pack(pady=5)
 
+    # 부분 레이블
+    tokenpocketLabel2 = Label(tokenpocketLabel, width=50, height=5, fg="red", relief="solid")
+    tokenpocketLabel2.pack(pady=5)
+
     # 파일 열기 버튼
-    button = Button(tokenpocketLabel, text="TokenPocket db 파일 열기", command=fileOpenDialog_TokenPocket, width=30, bg='yellow')
-    button.pack(pady=5)
+    button4 = Button(tokenpocketLabel2, text="Open TokenPocket db file", command=fileOpenDialog_TokenPocket, width=30, bg='yellow', state='disabled')
+    button4.pack(pady=5, side=LEFT)
+
+    def chk4proc():
+        if(var4.get()==1):
+            button4.config(state='active')
+        else:
+            button4.config(state='disabled')   
+    
+    # check box
+    var4 = IntVar()
+    c4 = Checkbutton(tokenpocketLabel2, text='', variable=var4, command=chk4proc)
+    c4.pack(side=RIGHT)   
 
     # TokenPocket 파일 경로 표시
-    tokenpocketFileLabel = Label(tokenpocketLabel, text="TokenPocket db 파일", width=40, height=3, fg="red", relief="solid")
+    tokenpocketFileLabel = Label(tokenpocketLabel, text="TokenPocket db file", width=40, height=3, fg="red", relief="solid")
     tokenpocketFileLabel.pack(pady=5)
 #=================================================================================
     
@@ -110,12 +173,27 @@ def main():
     enjinLabel = Label(window, width=50, height=10, fg="red", relief="solid")
     enjinLabel.pack(pady=5)
 
-    # 파일 열기 버튼
-    button = Button(enjinLabel, text="Enjin_Wallet db 파일 열기", command=fileOpenDialog_Enjin_Wallet, width=30, bg='yellow')
-    button.pack(pady=5)
+    # 부분 레이블
+    enjinLabel2 = Label(enjinLabel, width=50, height=5, fg="red", relief="solid")
+    enjinLabel2.pack(pady=5)      
 
-    # Bitget_Wallet 파일 경로 표시
-    enjinFileLabel = Label(enjinLabel, text="Enjin_Wallet db 파일", width=40, height=3, fg="red", relief="solid")
+    # 파일 열기 버튼
+    button5 = Button(enjinLabel2, text="Open Enjin_Wallet db file", command=fileOpenDialog_Enjin_Wallet, width=30, bg='yellow', state='disabled')
+    button5.pack(pady=5, side=LEFT)
+
+    def chk5proc():
+        if(var5.get()==1):
+            button5.config(state='active')
+        else:
+            button5.config(state='disabled')   
+    
+    # check box
+    var5 = IntVar()
+    c5 = Checkbutton(enjinLabel2, text='', variable=var5, command=chk5proc)
+    c5.pack(side=RIGHT)  
+
+    # Enjin_Wallet 파일 경로 표시
+    enjinFileLabel = Label(enjinLabel, text="Enjin_Wallet db file", width=40, height=3, fg="red", relief="solid")
     enjinFileLabel.pack(pady=5)
 #=================================================================================
     
@@ -128,58 +206,143 @@ def main():
     defiLabel = Label(window, width=50, height=10, fg="red", relief="solid")
     defiLabel.pack(pady=5)
 
-    # 파일 열기 버튼
-    button = Button(defiLabel, text="DeFi_Wallet db 파일 열기", command=fileOpenDialog_Defi_Wallet, width=30, bg='yellow')
-    button.pack(pady=5)
+    # 부분 레이블
+    defiLabel2 = Label(defiLabel, width=50, height=5, fg="red", relief="solid")
+    defiLabel2.pack(pady=5)
 
-    # Bitget_Wallet 파일 경로 표시
-    defiFileLabel = Label(defiLabel, text="DeFi_Wallet db 파일", width=40, height=3, fg="red", relief="solid")
+    # 파일 열기 버튼
+    button6 = Button(defiLabel2, text="Open DeFi_Wallet db file", command=fileOpenDialog_Defi_Wallet, width=30, bg='yellow', state='disabled')
+    button6.pack(pady=5, side=LEFT)
+
+    def chk6proc():
+        if(var6.get()==1):
+            button6.config(state='active')
+        else:
+            button6.config(state='disabled')   
+
+    # check box
+    var6 = IntVar()
+    c6 = Checkbutton(defiLabel2, text='', variable=var6, command=chk6proc)
+    c6.pack(side=RIGHT)        
+    
+    # DeFi_Wallet 파일 경로 표시
+    defiFileLabel = Label(defiLabel, text="DeFi_Wallet db file", width=40, height=3, fg="red", relief="solid")
     defiFileLabel.pack(pady=5)
 #=================================================================================    
 
-    # 분석 시작버튼 클릭
-  #  main = main()
+
+# 분석 시작버튼 클릭
     def btnpress():                            # 함수 btnpress() 정의
    
         print('button press')
 
-        # 00. 결과 label 초기화
-        resultlabel.configure(text = "분석 결과")
+        # 000. path 초기화
+       # window.exodusFilePath = ' '
+       # window.bitgetFilePath = ' '
+       # window.bitpayFilePath = ' '
+       # window.tokenpocketFilePath = ' '
+       # window.enjinFilePath = ' '
+       # window.defiFilePath = ' '
 
-        # 0. db파일들 및 csv 파일 저장 위치 설정
-        exodusFilePath = window.exodusFilePath
-        bitgetFilePath = window.bitgetFilePath
-        bitpayFilePath = window.bitpayFilePath
-        tokenpocketFilePath = window.tokenpocketFilePath
-        enjinFilePath = window.enjinFilePath
-        defiFilePath = window.defiFilePath
+        exodusFilePath = ' '
+        bitgetFilePath = ' '
+        bitpayFilePath = ' '
+        tokenpocketFilePath = ' '
+        enjinFilePath = ' '
+        defiFilePath = ' '
+                
+     #   resultlabel = Label(window, text = 'Analysis Reuslt', height=1)
+     #   resultlabel.pack(pady=5)
+
+        # 0. 앱 선택 checkbox check값 추출
+        checkList = [var1.get(), var2.get(), var3.get(), var4.get(), var5.get(), var6.get()]
+        
+        print(var1.get())
+        # 0. db파일들 경로 설정
+        if(checkList[0]==1):
+            exodusFilePath = window.exodusFilePath
+        if(checkList[1]==1):            
+            bitgetFilePath = window.bitgetFilePath
+        if(checkList[2]==1):            
+            bitpayFilePath = window.bitpayFilePath
+        if(checkList[3]==1):
+            tokenpocketFilePath = window.tokenpocketFilePath
+        if(checkList[4]==1):
+            enjinFilePath = window.enjinFilePath
+        if(checkList[5]==1):
+            defiFilePath = window.defiFilePath
+
+        FilePathList = [exodusFilePath, bitgetFilePath, bitpayFilePath, tokenpocketFilePath, enjinFilePath, defiFilePath]
+        print(FilePathList[5])
+
 
         # 1. 메인로직 함수 호출
-        mainlogic = MainLogic.MainLogic(exodusFilePath, bitgetFilePath, bitpayFilePath, tokenpocketFilePath, enjinFilePath, defiFilePath)
+        mainlogic = MainLogic.MainLogic(FilePathList, checkList)
         mainlogic.start()
 
         # 2. 결과 출력
-        resultlabel.configure(text = "분석 완료")
+        resultlabel.configure(text = "Analysis Complete")
+
+        # 3. reset 버튼 누르기 전에는 비활성화
+        btn.config(state='disabled')
 
 
     # 분석 및 CSV 추출 시작 버튼
     btn = Button(window)                       # window라는 창에 버튼 생성
-    btn.config(text= "분석 시작")              # 버튼 내용 
+    btn.config(text= "Start to Analyze")              # 버튼 내용 
     btn.config(width=30)                       # 버튼 크기
     btn.config(command=btnpress)               # 버튼 함수 호출
     btn.config(bg='orange')
     btn.pack(pady=10)                          # 버튼 배치
 
+
     # 분석 결과 (성공) 출력
-    resultlabel = Label(window, text = '분석 결과', height=1)
+    resultlabel = Label(window, text = 'Analysis Reuslt', height=1)
     resultlabel.pack(pady=5)
 
+    # 분석 초기화 버튼 클릭
+    def reset_btnpress():                            # 함수 btnpress() 정의
+        print('button press')
+        var1.set(0)
+        var2.set(0)
+        var3.set(0)
+        var4.set(0)
+        var5.set(0)
+        var6.set(0)
+
+        button1.config(state='disabled')
+        button2.config(state='disabled')
+        button3.config(state='disabled')
+        button4.config(state='disabled')
+        button5.config(state='disabled')
+        button6.config(state='disabled')
+
+        # 결과 label 초기화
+        resultlabel.configure(text = 'Analysis Reuslt')
+
+        # 분석 시작 버튼 활성화
+        btn.config(state='active')
+        
+
+    # 분석 초기화 버튼
+    reset_btn = Button(window)                       # window라는 창에 버튼 생성
+    reset_btn.config(text= "Reset")              # 버튼 내용 
+    reset_btn.config(width=10)                       # 버튼 크기
+    reset_btn.config(command=reset_btnpress)               # 버튼 함수 호출
+    reset_btn.config(bg='green')
+    reset_btn.pack(pady=10, side=BOTTOM)                          # 버튼 배치
+
     # 블로그 홈페이지 바로가기 버튼
-    openWebButton = Button(window, text = "ironmask 블로그 바로가기",command=openweb)
-    openWebButton.pack(side=BOTTOM, pady=5)
+#    openWebButton = Button(window, text = "Visit to ironmask blog",command=openweb)
+#    openWebButton.pack(side=BOTTOM, pady=5)
 
     window.mainloop()
 
 
+
+
 if __name__ == "__main__":
     main()
+
+
+
--- MainLogic.py
+++ MainLogic.py
@@ -18,53 +18,57 @@ import Defi
 
 class MainLogic:
 
-    def __init__(self, exodusFilePath, bitgetFilePath, bitpayFilePath, tokenpocketFilePath, enjinFilePath, defiFilePath):
+    # def __init__(self, exodusFilePath, bitgetFilePath, bitpayFilePath, tokenpocketFilePath, enjinFilePath, defiFilePath):
+    def __init__(self, FilePathList, checkList):
         print("Hello, Now is " + time.ctime() + "\n")
 
-        self.exodusFilePath = exodusFilePath
-        self.bitgetFilePath = bitgetFilePath
-        self.bitpayFilePath = bitpayFilePath
-        self.tokenpocketFilePath = tokenpocketFilePath
-        self.enjinFilePath = enjinFilePath
-        self.defiFilePath = defiFilePath
+        self.exodusFilePath = FilePathList[0]
+        self.bitgetFilePath = FilePathList[1]
+        self.bitpayFilePath = FilePathList[2]
+        self.tokenpocketFilePath = FilePathList[3]
+        self.enjinFilePath = FilePathList[4]
+        self.defiFilePath = FilePathList[5]
+
+        self.checkList = checkList
         
-        print(exodusFilePath)
-        print(bitgetFilePath)
-        print(bitpayFilePath)
-        print(tokenpocketFilePath)
-        print(enjinFilePath)
-        print(defiFilePath)
+        print(self.exodusFilePath)
+        print(self.bitgetFilePath)
+        print(self.bitpayFilePath)
+        print(self.tokenpocketFilePath)
+        print(self.enjinFilePath)
+        print(self.defiFilePath)
 
 
     def start(self):
 
         # ===========================  Exodus Start =================================================
 
-        conn = sqlite3.connect(self.exodusFilePath)
+        if(self.exodusFilePath != ' ' and self.checkList[0] != 0):
+            conn = sqlite3.connect(self.exodusFilePath)
 
-        c = conn.cursor()
+            c = conn.cursor()
 
-        exodus = Exodus.Exodus(c)
-        exodus.proc_address()
-        exodus.proc_transaction()
-        exodus.proc_app_ins_time()
-        exodus.proc_app_start_time()
-
-
-        print(exodus.address)
-        print(exodus.dict_trans_list[0])
-        print(exodus.app_ins_time)
-        print(exodus.app_start_time)
-
-        #print(float(exodus.app_start_time + ".0000000"))
-        str = exodus.app_start_time
-        print(float(str[:10] + "." + str[10:]))
-
-        print(time.asctime(time.localtime(float(str[:10] + "." + str[10:]))))
+            exodus = Exodus.Exodus(c)
+            exodus.proc_address()
+            exodus.proc_transaction()
+            exodus.proc_app_ins_time()
+            exodus.proc_app_start_time()
 
 
-        c.close()
-        conn.close()
+            print(exodus.address)
+            print(exodus.dict_trans_list[0])
+            print(exodus.app_ins_time)
+            print(exodus.app_start_time)
+
+            #print(float(exodus.app_start_time + ".0000000"))
+            str = exodus.app_start_time
+            print(float(str[:10] + "." + str[10:]))
+
+            print(time.asctime(time.localtime(float(str[:10] + "." + str[10:]))))
+
+
+            c.close()
+            conn.close()
 
         # ===========================  Exodus End =================================================
 
@@ -72,24 +76,25 @@ class MainLogic:
         # ===========================  Bitkeep Start =================================================
 
      #   conn = sqlite3.connect('bitkeep_09_25.db')
-        conn = sqlite3.connect(self.bitgetFilePath)
+        if(self.bitgetFilePath != ' ' and self.checkList[1] != 0):
+            conn = sqlite3.connect(self.bitgetFilePath)
 
-        c = conn.cursor()
+            c = conn.cursor()
 
-        bitkeep = Bitkeep.Bitkeep(c)
-        bitkeep.proc_address()
-        bitkeep.proc_transaction()
-        bitkeep.proc_app_ins_time()
-        bitkeep.proc_wallet_name()
+            bitkeep = Bitkeep.Bitkeep(c)
+            bitkeep.proc_address()
+            bitkeep.proc_transaction()
+            bitkeep.proc_app_ins_time()
+            bitkeep.proc_wallet_name()
 
-        str = bitkeep.app_ins_time
+            str = bitkeep.app_ins_time
 
-        print('앱 설치 시간 - 유닉스타임 변환')
-        print(time.asctime(time.localtime(str)))
+            print('앱 설치 시간 - 유닉스타임 변환')
+            print(time.asctime(time.localtime(str)))
 
 
-        c.close()
-        conn.close()
+            c.close()
+            conn.close()
 
 
         # ===========================  Bitkeep End =================================================
@@ -98,24 +103,25 @@ class MainLogic:
         # ===========================  Bitpay Start =================================================
 
         #conn = sqlite3.connect('RKStorage_bitpay_10_05')
-        conn = sqlite3.connect(self.bitpayFilePath)
+        if(self.bitpayFilePath != ' ' and self.checkList[2] != 0):
+            conn = sqlite3.connect(self.bitpayFilePath)
 
-        c = conn.cursor()
+            c = conn.cursor()
 
-        bitpay = Bitpay.Bitpay(c)
-        bitpay.proc_privatekey()
-        bitpay.proc_mnemonic()
-        bitpay.proc_app_ins_time()
-        bitpay.proc_app_start_time()
-        bitpay.proc_wallet_name()
+            bitpay = Bitpay.Bitpay(c)
+            bitpay.proc_privatekey()
+            bitpay.proc_mnemonic()
+            bitpay.proc_app_ins_time()
+            bitpay.proc_app_start_time()
+            bitpay.proc_wallet_name()
 
-        str = bitpay.app_ins_time
-        print(float(str[:10] + "." + str[10:]))
+            str = bitpay.app_ins_time
+            print(float(str[:10] + "." + str[10:]))
 
-        print(time.asctime(time.localtime(float(str[:10] + "." + str[10:]))))
+            print(time.asctime(time.localtime(float(str[:10] + "." + str[10:]))))
 
-        c.close()
-        conn.close()
+            c.close()
+            conn.close()
 
 
         # ===========================  Bitpay End =================================================
@@ -124,16 +130,17 @@ class MainLogic:
         # ===========================  Token Pocket Start =================================================
 
         # conn = sqlite3.connect('tp_db_common_tokenpocket_10_05')
-        conn = sqlite3.connect(self.tokenpocketFilePath)
+        if(self.tokenpocketFilePath != ' ' and self.checkList[3] != 0):
+            conn = sqlite3.connect(self.tokenpocketFilePath)
 
-        c = conn.cursor()
+            c = conn.cursor()
 
-        tokenpocket = Tokenpocket.Tokenpocket(c)
-        tokenpocket.proc_address()
+            tokenpocket = Tokenpocket.Tokenpocket(c)
+            tokenpocket.proc_address()
 
 
-        c.close()
-        conn.close()
+            c.close()
+            conn.close()
 
 
         # ===========================  Token Pocket End ================================
@@ -142,18 +149,19 @@ class MainLogic:
         # ===========================  Enjin Wallet Start =================================================
 
         #conn = sqlite3.connect('2_enjin_10_05')
-        conn = sqlite3.connect(self.enjinFilePath)
+        if(self.enjinFilePath != ' ' and self.checkList[4] != 0):
+            conn = sqlite3.connect(self.enjinFilePath)
 
-        c = conn.cursor()
+            c = conn.cursor()
 
-        enjin = Enjin.Enjin(c)
-        enjin.proc_transaction()
+            enjin = Enjin.Enjin(c)
+            enjin.proc_transaction()
 
-        print(enjin.dict_trans_list)
-        print(enjin.dict_trans_list[0].get('time'))
+            print(enjin.dict_trans_list)
+            print(enjin.dict_trans_list[0].get('time'))
 
-        c.close()
-        conn.close()
+            c.close()
+            conn.close()
 
 
         # ===========================  Enjin Wallet End ======================================
@@ -162,17 +170,17 @@ class MainLogic:
         # ===========================  DeFi Wallet Start =================================================
 
         #conn = sqlite3.connect('crypto_wallet_defi_10_05.db')
-        conn = sqlite3.connect(self.defiFilePath)
-        
-        c = conn.cursor()
+        if(self.defiFilePath != ' ' and self.checkList[5] != 0):
+            conn = sqlite3.connect(self.defiFilePath)
+            
+            c = conn.cursor()
 
-        defi = Defi.Defi(c)
-        defi.proc_transaction()
-        defi.proc_wallet_name()
+            defi = Defi.Defi(c)
+            defi.proc_transaction()
+            defi.proc_wallet_name()
 
-
-        c.close()
-        conn.close()
+            c.close()
+            conn.close()
 
 
         # ===========================  DeFi Wallet End ================================
@@ -188,15 +196,16 @@ class MainLogic:
             #writer.writerows(result)
             writer.writerow(["Android Wallet Analyzer - CSV File Report"])
 
-            exodus.proc_csv(writer)
-
-            bitkeep.proc_csv(writer)
-            
-            bitpay.proc_csv(writer)
-
-            tokenpocket.proc_csv(writer)
-
-            enjin.proc_csv(writer)
-
-            defi.proc_csv(writer)
+            if(self.exodusFilePath != ' ' and self.checkList[0] != 0):
+                exodus.proc_csv(writer)
+            if(self.bitgetFilePath != ' ' and self.checkList[1] != 0):
+                bitkeep.proc_csv(writer)
+            if(self.bitpayFilePath != ' ' and self.checkList[2] != 0):
+                bitpay.proc_csv(writer)
+            if(self.tokenpocketFilePath != ' ' and self.checkList[3] != 0):
+                tokenpocket.proc_csv(writer)
+            if(self.enjinFilePath != ' ' and self.checkList[4] != 0):
+                enjin.proc_csv(writer)
+            if(self.defiFilePath != ' ' and self.checkList[5] != 0):
+                defi.proc_csv(writer)
 
